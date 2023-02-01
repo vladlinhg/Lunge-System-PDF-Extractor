a387c771d9a3aa34ea60aa7a44df44615429749d
vladlinhg <victor.lin.hg@gmail.com>
2023-02-01 00:50:12 -0800

    mysql connector implemented

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Lunge-System-PDF-Extractor 
- It's an application extracting contents of pdf files into string format, and the extracted data will be stored into MySQL, json, or txt files
+    It's an application extracting contents of pdf files into string format, and the extracted data will be stored into MySQL, json, or txt files
+# Developer Team
+    Nai Yen Lin
+    Eddie Wang
+# Installation & Environment
+
+
+# Features
---
+++ pdf_scraping.py
@@ -0,0 +1,54 @@
+"""
+    An application can extract content in pdf file into mysql server and store them
+"""
+import getpass
+import mysql.connector
+
+
+class Database:
+    def __init__(self) -> None:
+        self.hostname = input("What host to connect to: ")
+        self.user = input("What user to connect with: ")
+        self.password = getpass.getpass("What password to connect with: ")
+
+
+class MyDB:
+    def __init__(self, db: Database):
+        """
+            MyDB is a class represent an instance of mysql.connector
+
+        Args:
+            db (Database): Database is the class stored information to login into specific MySQL server 
+        """    
+        try:
+            res = mysql.connector.connect(
+                host=str(db.hostname),
+                user=str(db.user),
+                password=str(db.password)
+            )
+            print("Connect successfully.")
+
+            self.db = res
+            self.cursor = self.db.cursor()
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+    
+    def show_databases(self) -> None:
+        self.cursor.execute("show databases")
+        print([row for row in self.cursor.fetchall()])
+
+    def create_databases(self) -> None:
+        pass
+    
+
+
+
+def main():
+    db = Database()
+    mydb = MyDB(db)
+    mydb.show_databases()
+
+
+if __name__ == "__main__":
+    main()
