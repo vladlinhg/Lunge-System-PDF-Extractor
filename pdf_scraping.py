d2e3e32fed3cc4ae49cd010fabb9dae8c5562a45
vladlinhg <victor.lin.hg@gmail.com>
2023-02-02 10:32:23 -0800

    Update text extraction function

--- pdf_scraping.py
+++ pdf_scraping.py
@@ -2,6 +2,8 @@
     An application can extract content in pdf file into mysql server and store them
 """
 import getpass
+from pathlib import Path
+import pdfplumber
 import mysql.connector
 
 
@@ -44,17 +46,34 @@ class MyDB:
     
     def create_table(self) -> None:
         self.cursor.execute("use lunge_test")
-        self.cursor.execute("create table articles (string ")
+        pass
+
+class Directory:
+    def __init__(self) -> None:
+        dir = input("Please Enter the directory in /xx/xx/xx or c:/xxx/xxx: ")
+        self.dir = Path(dir)
+
+class DirInput(Directory):
+    def __init__(self) -> None:
+        super().__init__()
+
+class DirOutput(Directory):
+    def __init__(self) -> None:
+        super().__init__()
 
     
 
 
 
 def main():
-    db = Database()
-    mydb = MyDB(db)
-    mydb.show_databases()
-    mydb.create_database()
+    dirinput = DirInput()
+    diroutput = DirOutput()
+    with pdfplumber.open(dirinput.dir) as pdf:
+        for page in pdf.pages:
+            text = page.extract_words(x_tolerance=3, y_tolerance=3, keep_blank_chars=False, use_text_flow=False, horizontal_ltr=True, vertical_ttb=True, extra_attrs=[], split_at_punctuation=False)
+            print(text)
+
+
 
 
 if __name__ == "__main__":
