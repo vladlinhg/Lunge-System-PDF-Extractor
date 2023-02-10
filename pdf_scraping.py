5eeab11b3b1c2cdc47bea8fc2afca90de8c16720
Eddie <eddie0906@hotmail.com>
2023-02-10 11:09:34 -0800

    Added new code

--- pdf_scraping.py
+++ pdf_scraping.py
@@ -4,9 +4,8 @@
 import getpass
 from pathlib import Path
 import pdfplumber
-import mysql.connector
+#import mysql.connector
 import json
-
 
 class Database:
     def __init__(self) -> None:
@@ -50,40 +49,64 @@ class MyDB:
         pass
 
 class Directory:
-    def __init__(self) -> None:
-        dir = input("Please Enter the directory in /xx/xx/xx or c:/xxx/xxx: ")
+    def init(self) -> None:
+        dir = input()
         self.dir = Path(dir)
 
 class DirInput(Directory):
-    def __init__(self) -> None:
-        dir = input("Please Enter the directory for data input in /xx/xx/xx.pdf or c:/xxx/xxx.pdf: ")
+    def init(self) -> None:
+        dir = input("Please enter the full pdf path for data input: ")
         self.dir = Path(dir)
 
 class DirOutput(Directory):
-    def __init__(self) -> None:
-        dir = input("Please Enter the directory for data output in /xx/xx/xx.json or c:/xxx/xxx.json: ")
+    def init(self) -> None:
+        dir = input("Please enter the full .json / .txt path for output: ")
         self.dir = Path(dir)
-
     
-
-
 
 def main():
     dirinput = DirInput()
+    dirinput.init()
     diroutput = DirOutput()
+    diroutput.init()
     content = []
     with pdfplumber.open(dirinput.dir) as pdf:
+        full_text = ""
+        #extracting text with pdfplumber, initialize full text variable to extract to.
         for page in pdf.pages:
-            text = page.extract_words(x_tolerance=3, y_tolerance=3, keep_blank_chars=False, use_text_flow=False, horizontal_ltr=True, vertical_ttb=True, extra_attrs=[], split_at_punctuation=False)
-            for word in text:
-                content.append(word["text"])
+            full_text += page.extract_text(
+                x_tolerance=3,
+                y_tolerance=3,
+                keep_blank_chars=False,
+                use_text_flow=False,
+                horizontal_ltr=True,
+                vertical_ttb=True,
+                extra_attrs=[],
+                split_at_punctuation=False
+            )
+            #Split string with newline seperator
+        paragraphs = full_text.split("\n")
+        #Initialize paragraph count and variable
+        current_paragraph = ""
+        paragraph_number = 1
+        for paragraph in paragraphs:
+            #Strip whitespace
+            if paragraph.strip() == "":
+                #Check if paragraph is empty. If it is, clear string and add paragraph count, append text
+                if current_paragraph != "":
+                    content.append({"paragraph_number": paragraph_number, "text": current_paragraph})
+                    current_paragraph = ""
+                    paragraph_number += 1
+            else:
+                #Append current paragraph to paragraph
+                current_paragraph += " " + paragraph
+        if current_paragraph != "":
+            #Once all paragraphs are processed, append to the content list
+            content.append({"paragraph_number": paragraph_number, "text": current_paragraph})
     jsonString = json.dumps(content)
     jsonFile = open(diroutput.dir, "w")
     jsonFile.write(jsonString)
-    jsonFile.close()   
-
-
-
-
+    jsonFile.close()
+        
 if __name__ == "__main__":
     main()
